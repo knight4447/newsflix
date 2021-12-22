e39e44d3a7c597fc7089c79794f5ab0706ef63c6
knight4447 <90621538+knight4447@users.noreply.github.com>
2021-12-22 23:27:13 +0530

    newsflix
    bbbbbbbbbbbb

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "814d5684df1e6b260f20fee976b07fd7";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
